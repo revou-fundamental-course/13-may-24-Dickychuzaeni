c31338fc9ad9c794a6e3b963cc3bdf295f07be91
dicky chuzaeni <dickychuzaeni044@gmail.com>
2024-05-23 00:38:24 +0700

    segitiga

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function validation(AlasValue,){}
+    let Alasvalue = document.getElementById('Alas-input').value;
+    let Tinggivalue = document.getElementById('Tinggi-input').value;
+    console.log(AlasValue);
+    console.log(TinggiValue);
+
+    if (AlasValue != '' && TinggiValue != ''){
+        console.log('sukses validasi');
+    }else{
+alert('inputan kosong');
+    }
+
